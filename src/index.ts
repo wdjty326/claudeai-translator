af01c24442a6e9c590aa1e0ac7268873dbe3afd5
정택유 <wdjty326@gmail.com>
2025-04-13 23:47:50 +0900

    번역 로직 수정

--- src/index.ts
+++ src/index.ts
@@ -17,24 +17,42 @@ async function main() {
         return;
     }
 
-    const messages: Anthropic.Messages.MessageParam[] = []
-    messages.push({ "role": "user", "content": "Please maintain the format of the text document, and translate only the Japanese text into Korean." })
+    if (!translatePath) {
+        console.error('TRANSLATE_PATH is not set');
+        return;
+    }
+
+    if (!fs.existsSync(translatePath)) {
+        fs.mkdirSync(translatePath);
+    }
+
+    const maxTokens = 1024;
 
     fs.readdirSync(extractPath).forEach(async (file) => {
+        const messages: Anthropic.Messages.MessageParam[] = []
+        messages.push({ "role": "user", "content": "Please maintain the format of the text document, and translate only the Japanese text into Korean." })
+    
         const content = fs.readFileSync(`${extractPath}/${file}`, 'utf-8');
+
         let i = 0;
         while (i < content.length) {
-            messages.push({ "role": "user", "content": content.slice(i, i + 1000) })
-            i += 1000;
+            messages.push({ "role": "user", "content": content.slice(i, i + 3000) })
+
+            const message = await client.messages.create({
+                max_tokens: maxTokens,
+                messages: messages,
+                model: 'claude-3-5-sonnet-latest',
+            });
+
+            const isFile = fs.existsSync(`${translatePath}/${file}`);
+            if (!isFile) {
+                fs.writeFileSync(`${translatePath}/${file}`, '');
+            }
+            fs.appendFileSync(`${translatePath}/${file}`, message.content[0].type === 'text' ? message.content[0].text : '');
+
+            i += 3000;
         }
 
-        const message = await client.messages.create({
-            max_tokens: 1024,
-            messages: messages,
-            model: 'claude-3-5-sonnet-latest',
-        });
-
-        fs.writeFileSync(`${file}`, message.content[0].type === 'text' ? message.content[0].text : '');
     })
 }
 
