73cff84c7277559fe69e71760bc552741877e09c
정택유 <wdjty326@gmail.com>
2025-04-13 23:35:06 +0900

    기본 번역 체크

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,41 @@
 import Anthropic from '@anthropic-ai/sdk';
 import dotenv from 'dotenv';
+import fs from 'fs';
 
 dotenv.config();
 
+const extractPath = process.env['EXTRACT_PATH'];
+const translatePath = process.env['TRANSLATE_PATH'];
+
 const client = new Anthropic({
-  apiKey: process.env['ANTHROPIC_API_KEY'], // This is the default and can be omitted
+    apiKey: process.env['ANTHROPIC_API_KEY'], // This is the default and can be omitted
 });
 
 async function main() {
-  const message = await client.messages.create({
-    max_tokens: 1024,
-    messages: [{ role: 'user', content: 'Hello, Claude' }],
-    model: 'claude-3-5-sonnet-latest',
-  });
+    if (!extractPath) {
+        console.error('EXTRACT_PATH is not set');
+        return;
+    }
 
-  console.log(message.content);
+    const messages: Anthropic.Messages.MessageParam[] = []
+    messages.push({ "role": "user", "content": "Please maintain the format of the text document, and translate only the Japanese text into Korean." })
+
+    fs.readdirSync(extractPath).forEach(async (file) => {
+        const content = fs.readFileSync(`${extractPath}/${file}`, 'utf-8');
+        let i = 0;
+        while (i < content.length) {
+            messages.push({ "role": "user", "content": content.slice(i, i + 1000) })
+            i += 1000;
+        }
+
+        const message = await client.messages.create({
+            max_tokens: 1024,
+            messages: messages,
+            model: 'claude-3-5-sonnet-latest',
+        });
+
+        fs.writeFileSync(`${file}`, message.content[0].type === 'text' ? message.content[0].text : '');
+    })
 }
 
 main();
